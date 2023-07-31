bb4b93b0964364a5e1024df7daaaf8164610d6a9
adityakamble12 <101994998+adityakamble12@users.noreply.github.com>
2023-07-31 18:12:33 +0530

    provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,7 @@
 # Configure the AWS Provider
 provider "aws" {
+  shared_config_files      = ["/home/ubuntu/.aws/conf"]
+  shared_credentials_files = ["/home/ubuntu/.aws/creds"]
+  profile                  = "Terraform-project"
   region                   = "us-east-2"
 }
