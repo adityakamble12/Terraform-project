06c81dfa774599318b575e93604e7c1cb3bf25bd
adityakamble12 <101994998+adityakamble12@users.noreply.github.com>
2023-08-02 13:02:13 +0530

    TERRAFORM AWS-VPC INFRA

--- provider.tf
+++ provider.tf
@@ -13,5 +13,5 @@ provider "aws" {
   region                   = "us-east-2"
   shared_config_files      = ["/home/ubuntu/.aws/config"]
   shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
-  profile                  = "Terraform-project"
+  profile                  = "Aditya_Kamble"
 }
